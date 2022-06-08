292af06698cbd7d529d4cce973a6adc236164119
azhuang321 <63149232+azhuang321@users.noreply.github.com>
2022-06-08 13:48:28 +0800

    10.注册页面开发

--- jingdong/src/router/index.js
+++ jingdong/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/home/Home';
 import Login from '../views/login/Login';
+import Register from '../views/register/Register';
 
 const routes = [
   {
@@ -12,6 +13,15 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+    beforeEnter (to, from, next) {
+      const { isLogin } = localStorage;
+      isLogin ? next({ name: 'Home' }) : next();
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
     beforeEnter (to, from, next) {
       const { isLogin } = localStorage;
       isLogin ? next({ name: 'Home' }) : next();
@@ -34,7 +44,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const { isLogin } = localStorage;
-  (isLogin || to.name === 'Login') ? next() : next({ name: 'Login' });
+  (isLogin || to.name === 'Login' || to.name === 'Register') ? next() : next({ name: 'Login' });
 });
 
 export default router;
--- jingdong/src/views/register/Register.vue
+++ jingdong/src/views/register/Register.vue
@@ -1,58 +1,64 @@
 <template>
   <div class="wrapper">
-    <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png" alt="">
+    <img class="wrapper__img" src="http://www.dell-lee.com/imgs/vue3/user.png"/>
     <div class="wrapper__input">
-      <input class="wrapper__input__content" placeholder="请输入手机号码" >
+      <input class="wrapper__input__content" placeholder="请输入手机号" />
     </div>
     <div class="wrapper__input">
-      <input class="wrapper__input__content" type="password" placeholder="请输入密码" >
+      <input
+        class="wrapper__input__content"
+        placeholder="请输入密码"
+        type="password"
+      />
     </div>
-    <div class="wrapper__login-button" @click="handleLogin">登陆</div>
-    <div class="wrapper__login-link" @click="handleRegisterClick">立即注册</div>
+    <div class="wrapper__input">
+      <input
+        class="wrapper__input__content"
+        placeholder="确认密码"
+        type="password"
+      />
+    </div>
+    <div class="wrapper__register-button">注册</div>
+    <div class="wrapper__register-link" @click="handleLoginClick">已有账号去登陆</div>
   </div>
 </template>
 
 <script>
 import { useRouter } from 'vue-router';
-
 export default {
-  name: 'Login',
+  name: 'Register',
   setup () {
     const router = useRouter();
-    const handleLogin = () => {
-      localStorage.isLogin = true;
-      router.push({ name: 'Home' });
+    const handleLoginClick = () => {
+      router.push({ name: 'Login' });
     };
-    const handleRegisterClick = () => {
-      router.push({ name: 'Register' });
-    };
-    return { handleLogin, handleRegisterClick };
+    return { handleLoginClick };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-@import "../../style/viriables";
-.wrapper{
+@import '../../style/viriables.scss';
+.wrapper {
   position: absolute;
   top: 50%;
   left: 0;
   right: 0;
   transform: translateY(-50%);
-  &__img{
+  &__img {
     display: block;
     margin: 0 auto .4rem auto;
     width: .66rem;
     height: .66rem;
   }
-  &__input{
+  &__input {
     height: .48rem;
     margin: 0 .4rem .16rem .4rem;
     padding: 0 .16rem;
     background: #F9F9F9;
-    border: 1px solid rgba(0,0,0,0.1);
+    border: 1px solid rgba(0,0,0,0.10);
     border-radius: 6px;
-    &__content{
+    &__content {
       line-height: .48rem;
       border: none;
       outline: none;
@@ -65,7 +71,7 @@ export default {
       }
     }
   }
-  &__login-button{
+  &__register-button {
     margin: .32rem .4rem .16rem .4rem;
     line-height: .48rem;
     background: #0091FF;
@@ -75,11 +81,10 @@ export default {
     font-size: .16rem;
     text-align: center;
   }
-  &__login-link{
+  &__register-link {
     text-align: center;
-    font-size: .16rem;
+    font-size: .14rem;
     color: $content-notice-fontcolor;
   }
 }
-
 </style>
