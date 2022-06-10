01ff58f8c85f81c8a7629aab018b653fe79fe831
azhuang321 <63149232+azhuang321@users.noreply.github.com>
2022-06-10 12:10:23 +0800

    17.完成首页 附近商家页面

--- jingdong/src/utils/request.js
+++ jingdong/src/utils/request.js
@@ -1,9 +1,24 @@
 import axios from 'axios';
 
+const axiosInstance = axios.create({
+  baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
+  timeout: 10000
+});
+
+
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    axiosInstance.get(url, { params }).then((resp) => {
+      resolve(resp.data);
+    }, err => {
+      reject(err);
+    });
+  });
+};
+
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
-    axios.post(url, data, {
-      baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
+    axiosInstance.post(url, data, {
       headers: {
         'Content-Type': 'application/json'
       }
